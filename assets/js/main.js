b69fa1460e4521d5bb8784f74d7e05653a1dae9d
gandasipayung <gandasipayung14@gmail.com>
2020-01-10 10:21:21 +0700

    Login kelar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+let tampung
 // login
 let google_token = null
 function onSignIn(googleUser) {
@@ -10,9 +11,15 @@ function onSignIn(googleUser) {
       google_token
     }
   })
-  .done( user => {
-    console.log(user);
-    
+  .done( token => {
+    console.log(token);
+    //localstorage
+    localStorage.setItem('token', token)
+    tampung = token
+    getAnime()
+    toggleLogin(0)
+    togglePage(1)
+    //
   })
   .fail( err => {
     console.log(err);
@@ -22,6 +29,57 @@ function onSignIn(googleUser) {
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
-     console.log('User signed out.');
+    localStorage.removeItem('token')
+    tampung = null
+    console.log('User signed out.');
+    toggleLogin(1)
+    togglePage(0)
   });
 }
+
+function getAnime() {
+  const token = localStorage.getItem('token')
+  $.ajax({
+    method : 'GET',
+    url : 'http://localhost:3000/anime',
+    headers : {
+      token
+    }
+  })
+    .done( data => {
+      console.log(data, '...')
+    })
+    .fail(err => {
+      console.log(err);
+    })
+}
+
+function toggleLogin(n) {
+  if(n) {
+    $('#login-page').show()
+  } else {
+    $('#login-page').hide()
+  }
+}
+
+function togglePage(n) {
+  if(n) {
+    $('#page').show()
+  } else {
+    $('#page').hide()
+  }
+}
+
+$(document).ready(function(){
+  
+  const token = localStorage.getItem('token')
+  if(token) {
+    togglePage(1)
+    toggleLogin(0)
+    getAnime()
+  } else {
+    toggleLogin(1)
+    togglePage(0)
+  }
+
+})
